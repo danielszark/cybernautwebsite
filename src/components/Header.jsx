abc76303f4c92873821232a9623f6acab5a42423
LinneaEO <olixl@wwu.edu>
2025-06-04 15:22:18 -0700

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className="bg-blue-800 text-blue-100 py-10">
+    <header className="font-zing bg-blue-800 text-blue-100 py-10">
         <nav className="container mx-auto flex justify-between items-center">
-            <h1 className="text-4xl font-bold">What is my social media doing to me? Project Site</h1>
+            <h1 className="text-4xl font-bold">What is my social media doing to me? (Project Site)</h1>
             <ul className="flex text-2xl space-x-4">
                 <li>
                     <NavLink to="/" className={({ isActive }) => isActive ? 'font-bold text-yellow-200' : 'hover:text-gray-300'}>Home</NavLink>
