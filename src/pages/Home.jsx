10b8b0c4b832f249d97925eec77e832a5631ee3a
Andrew Essex <Andrew.Essex@stmartin.edu>
2025-06-04 17:47:33 -0700

    added profile pic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import emptyProfile from '../assets/emptyprofile.jpg'
+import emptyProfile from '../assets/emptyprofilepic.jpg'
+import andrewProfile from '../assets/andrewprofilepic.jpg'
 import linneaProfile from '../assets/linneaprofilepicture.jpg'
 
 const Home = () => {
@@ -22,7 +23,7 @@ const Home = () => {
         </div>
         {/*Developer 2*/}
         <div className="flex items-center space-x-4 bg-white p-4 rounded-xl w-full max-w-3xl">
-          <img src={emptyProfile} alt="Profile picture" className="w-24 h-24 rounded-full object-cover"/>
+          <img src={andrewProfile} alt="Profile picture" className="w-24 h-24 rounded-full object-cover"/>
             <div className="text-left">
               <h3 className="text-2xl font-bold text-[#275481]">Andrew Essex (Graduate)</h3>
               <h4 className="text-lg text-black">essexa@wwu.edu</h4>
